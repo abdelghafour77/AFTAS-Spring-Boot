3653b42d388f50b510bc721441c117a1edc94adc
abdelghafour <miso.abdo@gmail.com>
2023-12-13 09:42:17 +0100

    first commit

---
+++ src/main/java/com/example/aftasspringboot/AftasSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.aftasspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftasSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aftasspringboot/AftasSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aftasspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftasSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

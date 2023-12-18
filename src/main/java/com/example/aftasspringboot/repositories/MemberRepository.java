865af0cf9aad5e6f29354f2eb9fe13508ac16d38
abdelghafour <miso.abdo@gmail.com>
2023-12-18 21:19:46 +0100

    update member

--- src/main/java/com/example/aftasspringboot/repositories/MemberRepository.java
+++ src/main/java/com/example/aftasspringboot/repositories/MemberRepository.java
@@ -1,11 +1,14 @@
 package com.example.aftasspringboot.repositories;
 
+import com.example.aftasspringboot.entities.Competition;
 import com.example.aftasspringboot.entities.Member;
 import com.example.aftasspringboot.entities.enums.IdentityDocumentType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,5 +19,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Page<Member> findAll(Pageable pageable);
     List<Member> findAll(Specification<Member> specification);
     Optional<Member> findByIdentityNumber(String identityNumber);
-
+    @Query("SELECT c FROM Member c WHERE lower(c.firstName) LIKE lower(concat('%', :search, '%')) OR lower(c.lastName) LIKE lower(concat('%', :search, '%')) OR lower(c.identityNumber) LIKE lower(concat('%', :search, '%'))")
+    List<Member> findByCriteria(@Param("search") String search);
 }
